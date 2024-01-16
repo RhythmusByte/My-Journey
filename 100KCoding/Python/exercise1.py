605c4a2a26c265a891b5a0bf42ca50192c1fdbfc
Akhil Mahesh <akhilmahesh@proton.me>
2024-01-16 13:09:32 +0530

    Added comment

--- 100KCoding/Python/exercise1.py
+++ 100KCoding/Python/exercise1.py
@@ -1,2 +1,9 @@
 print('String Manipulation Exercise\nString concatenation is done with "+" sign.\nEg: print("Hello" + "World")')
 print('New lines can be created with a backslash and n.')
+
+# Output:
+
+# String Manipulation Exercise
+# String concatenation is done with "+" sign.
+# Eg: print("Hello" + "World")
+# New lines can be created with a backslash and n.
