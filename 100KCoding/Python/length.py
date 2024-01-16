8df04e1dcdaf8b7a572579844cb442b2afc6c947
Akhil Mahesh <akhilmahesh@proton.me>
2024-01-16 14:10:25 +0530

    Added alternative and comment

--- 100KCoding/Python/length.py
+++ 100KCoding/Python/length.py
@@ -1,4 +1,8 @@
 name = input("What is your name? ")
-print(len(name))
+length = len(name)
+print(length)
+
+# Alternatively we can use this:
+# print(len(name))
 
 # len() is a function to find the length
