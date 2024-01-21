3fd55e0ac4d401fccaf0afd7baea15605210b592
Akhil Mahesh <akhilmahesh@proton.me>
2024-01-21 12:52:30 +0530

    Fixed errors

--- 100KCoding/Python/swap.py
+++ 100KCoding/Python/swap.py
@@ -1,4 +1,11 @@
 a=input("Enter value of a = ")
 b=input("Enter value of b = ")
-print("a = " + b)
-print("b = " + a)
+
+print('Current values of a is ' + a + 'and value of b is ' + b)
+
+temp=a #temperory storing the value of a
+a=b #Now the a is free so given the value of b to a
+b=temp #Now the b is free so the value of temp is given to b
+
+print('a = ' + a)
+print('b = ' + b)
