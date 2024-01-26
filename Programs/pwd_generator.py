800fbcd262b25802179cf6f941318e1c3684fc53
Akhil Mahesh <akhilmahesh@proton.me>
2024-01-26 11:04:31 +0530

    Added comments and some lines

--- Programs/pwd_generator.py
+++ Programs/pwd_generator.py
@@ -7,15 +7,19 @@ symbols = "@#_&-+*!?[]{}"
 
 chars = lower + upper + numbers + symbols
 
+# Loop for keep running the program until the user exit
+
 while True:
     length = input("Enter the length of the password (or type 'exit' to stop): ")
 
     if length.lower() == 'exit':
         break
 
+    # Program stops if user input is "exit"
+    
     try:
         length = int(length)
         password = "".join(random.sample(chars, length))
         print("Generated Password:", password)
     except ValueError:
-        print("\nInvalid input. Please enter a valid integer for the password length.")
+        print("Invalid input. Please enter a valid number for the password length. (Eg: 6)")
