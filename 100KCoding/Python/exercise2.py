8b0d06d9252ba6777768aa349726492f4f6bc214
Akhil Mahesh <akhilmahesh@proton.me>
2024-01-27 09:26:29 +0530

    Create exercise2.py

---
+++ 100KCoding/Python/exercise2.py
@@ -0,0 +1,9 @@
+digits = input("Enter a 2 digit number (Eg: 12) : ")
+
+first = digits[0]
+second = digits[1]
+
+sum = int(first) + int(second)
+sum = str(sum)
+
+print(first + "+" + second + "=" + sum)
