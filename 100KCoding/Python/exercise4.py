e358d5a4de4b15eaf763c27e591d3561c82b3c1f
RhythmusByte <AkhilMahesh@proton.me>
2024-02-24 11:02:30 +0530

    Shortened Code

--- 100KCoding/Python/exercise4.py
+++ 100KCoding/Python/exercise4.py
@@ -2,7 +2,6 @@ print('Finding BMI = weight/height²')
 weight = int(input("What is your weight?\t"))
 height = float(input('What is your height in metres?\t'))
 
-h = height * height
-BMI = weight/h
+BMI = weight/height ** 2
 
 print(int(BMI))
