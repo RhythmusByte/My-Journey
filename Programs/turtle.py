3de0b3aff8d7ffd3bdb01a474c569f41a42c2726
RhythmusByte <AkhilMahesh@proton.me>
2024-02-24 10:15:19 +0530

    New Programs

---
+++ Programs/turtle.py
@@ -0,0 +1,13 @@
+import turtle
+
+# Initialize the screen
+screen = turtle.Screen()
+
+# Initialize a turtle object
+my_turtle = turtle.Turtle()
+
+# Write the message
+my_turtle.write("Welcome to Python")
+
+# Keep the window open
+turtle.done()
