058cf8cb90ceac93434e7e7ea5f113d2cbb7b7f8
Akhil Mahesh <akhilmahesh@proton.me>
2024-03-05 10:16:27 +0530

    Create something.py

---
+++ Programs/something.py
@@ -0,0 +1,13 @@
+# String Example 
+name = "John"
+# Integer Example
+age = 25 
+# Float Example 
+height = 1.75 
+# Boolean Example
+is_student = True
+# Print variable values 
+print("Name:", name) 
+print("Age:", age) 
+print("Height:", height)
+print("Is student?", is_student)
