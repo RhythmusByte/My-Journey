c7a82c7ca456b51521fe45297bddeb71d463a270
RhythmusByte <AkhilMahesh@proton.me>
2024-03-13 08:18:02 +0530

    New Exercise

---
+++ 100KCoding/Python/exercise5.py
@@ -0,0 +1,8 @@
+age=int(input('Enter your age: '))
+
+years = 90 - age
+days = years * 365
+months = years * 12
+weeks = years * 52
+
+print(f'You have {days} days, {weeks} weeks, {months} months and {years} years left')
