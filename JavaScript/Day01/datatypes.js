e57ef5dad55ede05fc0872ea5d25473f40b20337
Akhil Mahesh <akhilmahesh@proton.me>
2024-03-16 16:54:59 +0530

    Exercise 5 of Day01

---
+++ JavaScript/Day01/datatypes.js
@@ -0,0 +1,13 @@
+// Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+let user = 'Akhil'
+let ageOfUser = 19
+let student = true
+let kid = undefined
+let married = null
+
+console.log(typeof kid)
+console.log(typeof married)
+console.log(typeof user)
+console.log(typeof ageOfUser)
+console.log(typeof student)
