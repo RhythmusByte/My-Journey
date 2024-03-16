410cb59e5f3e7ecea45aaa8a93736d015cc2f359
Akhil Mahesh <akhilmahesh@proton.me>
2024-03-16 17:05:52 +0530

    Exercise 8

---
+++ JavaScript/Day01/data.js
@@ -0,0 +1,6 @@
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let firstName = 'Akhil'
+let lastName = 'Mahesh'
+let maritalStatus = false
+let country = 'India'
