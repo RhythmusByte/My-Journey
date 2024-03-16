40da7a386fa55215cf22e582f75d34d120b36845
Akhil Mahesh <akhilmahesh@proton.me>
2024-03-16 16:47:42 +0530

    Declared variables and assigned string, boolean, undefined and null data types

---
+++ JavaScript/Day01/variables.js
@@ -0,0 +1,7 @@
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+let firstName = 'Akhil'
+let age = 19
+let isStudent = true
+let kids = undefined
+let wife = null
