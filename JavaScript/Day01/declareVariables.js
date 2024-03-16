7e20b5a57d8fab40ec5dd882aea7afc8813b2da4
Akhil Mahesh <akhilmahesh@proton.me>
2024-03-16 17:00:47 +0530

    Exercise 6 & 7 in Day 1

---
+++ JavaScript/Day01/declareVariables.js
@@ -0,0 +1,13 @@
+// Declare four variables without assigning values
+
+let day
+let year
+let month
+let week
+
+// Declare four variables with assigned values
+
+let firstName = 'Akhil'
+let lastName = 'Mahesh'
+let age = 19
+let android= true
