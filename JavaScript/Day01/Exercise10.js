a26411330838e64b83fecdce1fb040b28b1199f1
Akhil Mahesh <akhilmahesh@proton.me>
2024-03-16 17:14:19 +0530

    Exercise 10

---
+++ JavaScript/Day01/Exercise10.js
@@ -0,0 +1,6 @@
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+let myAge = 19
+let yourAge = 20
+
+console.log('I am ', + myAge + ' years old.\nYou are ' + yourAge + ' years old.')
