e2cc797fb904e4271b76c62ae8e3094cc35d6dba
RhythmusByte <AkhilMahesh@proton.me>
2024-03-21 22:37:44 +0530

    New Dir for programs

---
+++ JavaScript/Others/comparison.js
@@ -0,0 +1,8 @@
+let x = "5";
+let y = 6;
+let equality = x == y;
+let greaterThan = x > y;
+let lessThan = x < y;
+let strictEquality = x === y;
+
+console.log(`The x is ${x} and y is ${y} \nx > y = ${greaterThan} \nx < y = ${lessThan} \nx is y = ${equality} \nIs x and y the same type? ${strictEquality}`);
