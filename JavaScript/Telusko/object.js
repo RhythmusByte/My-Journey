1ad5c00b37aa5d75b76aa51a15b7a6466c2fbd13
RhythmusByte <AkhilMahesh@proton.me>
2024-04-05 18:46:37 +0530

    object

---
+++ JavaScript/Telusko/object.js
@@ -0,0 +1,12 @@
+let person = {
+  name : 'Akhil',
+  age : 19,
+  gender : 'Male',
+  likes : {
+    games : 'bgmi',
+    sports : 'soccer',
+    book : 'It Ends With Us'
+  }
+};
+
+console.log(person);
