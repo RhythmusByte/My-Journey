64bfed5aa6f81fe2296ccd230a43db645b02e5f4
RhythmusByte <AkhilMahesh@proton.me>
2024-04-06 12:11:32 +0530

    Shortened code

--- JavaScript/Tasks/task2.js
+++ JavaScript/Tasks/task2.js
@@ -1,24 +1,4 @@
 // Fetch an object inside the nested object using for in loop
-console.log(`Fetch an object inside the nested object using for in loop`);
-
-console.log(`Source Code:
-let user = {
-  firstName: 'Akhil',
-  lastName: 'Mahesh',
-  age: 19,
-  tech: 'JavaScript',
-  hobbies: {
-    game: 'BGMI',
-    sports: 'Soccer',
-  },
-  gender: 'Male'
-};
-
-for(let key in user){
-  console.log(key, user[key]);
-}`);
-
-console.log(`\nOutput:\n`);
 
 let user = {
   firstName: 'Akhil',
