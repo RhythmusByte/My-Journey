7f9874ab5e93b7579e44bd0fe135a2a2ff9045d9
Akhil Mahesh <akhilmahesh@proton.me>
2024-04-14 17:20:03 +0530

    Update main.js

--- JavaScript/BroCode/Circumference-Of-A-Circle/main.js
+++ JavaScript/BroCode/Circumference-Of-A-Circle/main.js
@@ -5,5 +5,5 @@ document.getElementById("submit").onclick = function () {
   radius = document.getElementById("radius").value;
   radius = Number(radius);
   circumference = 2 * PI * radius;
-  document.getElementById("result").textContent = `Circumference of the circle is ${circumference}`;
+  document.getElementById("result").textContent = `Circumference of the circle is ${circumference} cm`;
 };
