6f5f3e3cec8b21e52e0bb638edfd01d2d6d994c2
Akhil Mahesh <akhilmahesh@proton.me>
2024-05-08 10:16:59 +0530

    Create nestedObject2.js

---
+++ JavaScript/BroCode/Others/nestedObject2.js
@@ -0,0 +1,17 @@
+let crew = [
+  {name: "Monkey D Luffy", position: "Captain", dfUser: true},
+  {name: "Roronoa Zoro", position: "Swordsman", dfUser: false},
+  {name: "Nami", position: "Navigator", dfUser: false},
+  {name: "Usop", position: "Sniper", dfUser: false},
+  {name: "Vinsmoke Sanji", position: "Cook", dfUser: false},
+  {name: "Tony Tony Chopper", position: "Doctor", dfUser: true},
+  {name: "Nico Robin", position: "Archaelogist", dfUser: true},
+  {name: "Franky", position: "Shipwright", dfUser: false},
+  {name: "Brook", position: "Musician", dfUser: true},
+  {name: "Jimbei", position: "Helmsman", dfUser: false}
+];
+
+crew.forEach(crew => console.log(crew.name, crew.position, crew.dfUser));
+crew.map(crew => console.log(crew.name));
+let dfUsers = crew.filter(crew => crew.dfUser);
+
