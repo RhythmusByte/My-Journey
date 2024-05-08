8c0b7875da1de23925324ee297f5b562df5d9775
Akhil Mahesh <akhilmahesh@proton.me>
2024-05-08 10:31:40 +0530

    Create sortingMethod.js

---
+++ JavaScript/BroCode/Others/sortingMethod.js
@@ -0,0 +1,9 @@
+let students = [
+  {name: "Honor", age: 15},
+  {name: "Samsung", age: 25},
+  {name: "Nokia", age: 30},
+  {name: "Apple", age: 70}
+];
+
+// console.log(students.sort((a, b) => a.age - b.age));
+// console.log(students.sort((a, b) => a.name.localeCompare(b.name)));
