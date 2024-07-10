11658caf0f8c132b070741d1ff58198f9438dc9d
RhythmusByte <akhilmahesh@proton.me>
2024-07-10 06:26:03 +0530

    30 Day Python

---
+++ Python/Day2/day2.py
@@ -0,0 +1,10 @@
+num1, num2 = 5, 4
+total = num1  +  num2
+diff = num2 - num1
+product =  num2 * num1
+division = num1/num2
+remainder = num2 % num1
+exp = num1 ** num2
+floor_division = num1 // num2
+
+print(num1, num2, diff, product, division, remainder, exp, floor_division)
